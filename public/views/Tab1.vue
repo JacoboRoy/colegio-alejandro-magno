36f1ed64847b1bfc3906038810509acea2e60031
Jacobo Roy <killerpollo22@msn.com>
2021-10-12 14:25:55 -0600

    prueba public vue ionic

---
+++ public/views/Tab1.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Colegio Alejandro Magno</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">"Conquistando el conocimiento de nuestro mundo"</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ExploreContainer name="Inicio" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  {
+  name: 'Tab1',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+}
+</script>
